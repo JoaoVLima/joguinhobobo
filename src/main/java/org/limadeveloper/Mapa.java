41492d6974a93bd21b1b17a8bcb79cc16fdd7ce9
JoaoVLima <celu.joaolima@gmail.com>
2025-09-17 22:27:02 -0300

    ler arquivo

--- src/main/java/org/limadeveloper/Mapa.java
+++ src/main/java/org/limadeveloper/Mapa.java
@@ -1,5 +1,9 @@
 package org.limadeveloper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Mapa {
     private char[][] matriz;
     private int numeroLinhas;
@@ -26,6 +30,12 @@ public class Mapa {
     }
 
     public void lerArquivo(String nomeArquivo){
-
+        try (Scanner scanner = new Scanner(new File(nomeArquivo))) {
+            while (scanner.hasNext()) {
+                System.out.println(scanner.next()); // Reads token by token
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Mapa não encontrada " + e.getMessage());
+        }
     }
 }
